9c8c816f8212460a23fac984d95ce487e87d3323
Iain MacLeod <iain.macleod@codepush.co.uk>
2023-01-21 20:44:27 +0000

    more initial setup

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/irmac/q9s
+
+go 1.19
+
+require github.com/go-stomp/stomp v2.1.4+incompatible
+
+require (
+	github.com/Azure/go-amqp v0.18.1 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,41 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+)
 
-	println("Hello, world!")
+func main() {
+	ctx := context.TODO()
+
+	conn, err := amqp.Dial("amqp://admin:admin@localhost:5672/", amqp.ConnSASLPlain("admin", "admin"))
+	if err != nil {
+		log.Fatalf("Failed to connect to ActiveMQ: %v", err)
+	}
+	defer conn.Close()
+
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatalf("Failed to create a session: %v", err)
+	}
+	defer session.Close(ctx)
+
+	receiver, err := session.NewReceiver(
+		amqp.LinkSourceAddress("test-queue"),
+		amqp.LinkCredit(10),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create a receiver: %v", err)
+	}
+	defer receiver.Close(ctx)
+
+	for i := 0; i < 10; i++ {
+		msg, err := receiver.Receive(ctx)
+		if err != nil {
+			log.Fatalf("Failed to receive a message: %v", err)
+		}
+
+		fmt.Println("Received message:", string(msg.GetData()))
+	}
 }
